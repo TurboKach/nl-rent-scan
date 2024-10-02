e95a62c2abf463eba6c1116b73f3a06b7ef4e10d
TurboKach <incognito@turbokach.me>
2024-10-02 02:00:20 +0200

    updated Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,34 +2,41 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
-# Update package lists
+# Update package lists and install core packages
 #echo "Updating package lists..."
 #apt-get update
 
-# Install dependencies for Chrome
-echo "Installing dependencies..."
-apt-get install -y wget gnupg2 software-properties-common unzip
+# Install basic dependencies
+echo "Installing basic dependencies..."
+apt-get install -y --no-install-recommends \
+    wget \
+    gpgv \
+    curl \
+    ca-certificates \
+    apt-transport-https \
+    gnupg \
+    unzip
 
 # Add Google Chrome's signing key
 echo "Adding Google Chrome signing key..."
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 
 # Add Chrome's stable repository
 echo "Adding Google Chrome stable repository..."
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 
-# Update package lists again
+# Update package lists for Chrome
 echo "Updating package lists for Chrome..."
 apt-get update
 
 # Install Google Chrome
 echo "Installing Google Chrome..."
-apt-get install -y google-chrome-stable
+apt-get install -y --no-install-recommends google-chrome-stable
 
 # Download ChromeDriver
 echo "Downloading ChromeDriver..."
 CHROME_VERSION=$(google-chrome --version | grep -oP "\d+\.\d+\.\d+")
-CHROMEDRIVER_VERSION=$(wget -qO- "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+CHROMEDRIVER_VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)
 wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip
 
 # Unzip ChromeDriver
